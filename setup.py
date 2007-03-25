9a25fca55d943ecc9402132240bb2d6be713b267
Bernd Dorn <bernddorn@gmail.com>
2007-03-25 10:12:57 +0000

    zope.app.zcmlfiles proj

---
+++ setup.py
@@ -0,0 +1,37 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.zcmlfiles package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+setup(name='zope.app.zcmlfiles',
+      version='3.4dev',
+      url='http://svn.zope.org/zope.app.zcmlfiles',
+      license='ZPL 2.1',
+      description='Zope zcmlfiles',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope', 'zope.app'],
+      install_requires=['setuptools',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
