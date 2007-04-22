d74ab8c516da691c67ca355d289a3dac8faac021
Christian Theune <ct@gocept.com>
2007-04-22 10:53:55 +0000

    Updating version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='zope.app.zcmlfiles',
-      version='3.4dev',
+      version = '3.4.0a1',
       url='http://svn.zope.org/zope.app.zcmlfiles',
       license='ZPL 2.1',
       description='Zope zcmlfiles',
