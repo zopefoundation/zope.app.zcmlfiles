08520697771bb6d511121736cd39bf96099ee203
Baiju Muthukadan <baiju.m.mail@gmail.com>
2007-10-03 06:38:03 +0000

    bump version number after release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ long_description = (
 
 setup(
     name='zope.app.zcmlfiles',
-    version='3.4.0',
+    version='3.4.1dev',
     url='http://pypi.python.org/pypi/zope.app.zcmlfiles',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
