cb2ca7e1fff2847cc0d5541be6999c9d84aea8ec
Fred Drake <fdrake@acm.org>
2007-10-23 23:45:04 +0000

    set version for release (3.4.1)

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ long_description = (
 
 setup(
     name='zope.app.zcmlfiles',
-    version='3.4.1dev',
+    version='3.4.1',
     url='http://pypi.python.org/pypi/zope.app.zcmlfiles',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
