ae70bd8ef1ffa5e2c7b35ba0a9e1273a414f81d3
Dan Korostelev <nadako@gmail.com>
2008-12-16 08:55:43 +0000

    vb

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ long_description = (
 
 setup(
     name='zope.app.zcmlfiles',
-    version='3.5.0dev',
+    version='3.5.1dev',
     url='http://pypi.python.org/pypi/zope.app.zcmlfiles',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
