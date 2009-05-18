d2b20bccaaa088c67ccf3bb9ae3becceb5218f0b
Michael Howitz <mh@gocept.com>
2009-05-18 17:29:45 +0000

    Back to development: 3.5.5dev

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ long_description = (
 
 setup(
     name='zope.app.zcmlfiles',
-    version = '3.5.4',
+    version = '3.5.5dev',
     url='http://pypi.python.org/pypi/zope.app.zcmlfiles',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
