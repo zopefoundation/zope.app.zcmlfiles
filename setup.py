9f558f00106bfd2a3d8b4c7116d2aef14f4c6993
Michael Howitz <icemac@gmx.net>
2023-07-10 08:16:22 +0200

    Back to development: 5.1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ long_description = (
 
 setup(
     name='zope.app.zcmlfiles',
-    version='5.0',
+    version='5.1.dev0',
     url='http://github.com/zopefoundation/zope.app.zcmlfiles',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
